139021672c27f40da8414b423ff8fc04ed363470
TJ Hooker <tyrelhooker@gmail.com>
2020-04-10 22:28:01 -0400

    Create initial file structure using create-react-app and prepare app.js for step by step react hooks implementation.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState({ hits: [] });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {data.hits.map(item => {
+        <li key={item.objectId}>
+          <a href={item.url}>{item.title}</a>
+        </li>
+      })}
+    </ul>
   );
 }
 
