34c36a74963d964e22da6a8afb17aa255797876d
TJ Hooker <tyrelhooker@gmail.com>
2020-04-11 22:06:14 -0400

    Create react hook to save api result in state. Refactor app to use browser native fetch instead of Axios

--- src/App.js
+++ src/App.js
@@ -1,17 +1,73 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
 import './App.css';
 
+const USER_SERVICE_URL = 'https://plantsdb.xyz/search?limit=10';
+
+
 function App() {
-  const [data, setData] = useState({ hits: [] });
-  return (
-    <ul>
-      {data.hits.map(item => {
-        <li key={item.objectId}>
-          <a href={item.url}>{item.title}</a>
-        </li>
-      })}
-    </ul>
-  );
+  const [error, setErrors] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [plants, setPlants] = useState({ myPlants: [] });
+
+  const fetchData = async () => {
+    const result = await fetch(USER_SERVICE_URL)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setPlants({ myPlants: result.data });
+        },
+
+        (error) => {
+          setIsLoaded(true);
+          setErrors(error);
+        }
+      )
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  
+
+  // const [id] = data.hits.data;
+  if (error) {
+    return <div>Error: {error.message}</div>
+  } else if (!isLoaded) {
+    return <div>Loading...</div>
+  } else {
+    return (
+      <div>
+
+        {console.log(plants.myPlants.map(item => item))}
+
+        <ul>
+          {plants.myPlants.map((item) => 
+            <li key={item.id}>
+              {item.Scientific_Name_x}
+              
+              <ul>
+                <li>
+                  {`${item.Common_Name}` ? `Common Name: ${item.Common_Name}` : `Common Name: no common name`}
+                </li>
+                <li>
+                  {`Genus: ${item.Genus}`}
+                </li>
+                <li>
+                  {`Species: ${item.Species}`}
+                </li>
+                <li>
+                  {item.Duration ? `Duration: ${item.Duration}` : `Duration: Unknown`}
+                </li>
+              </ul>
+            </li>
+          )}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
